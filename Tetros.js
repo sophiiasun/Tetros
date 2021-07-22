ddcae7e6cb6c4b79670f86ea8e31133982957df4
tmaswang69 <thomaswang003@gmail.com>
2021-07-22 19:19:22 -0400

    Back-end

---
+++ Tetros.js
@@ -0,0 +1,16 @@
+function Coordinates(x, y){
+    this.x = x; this.y = y; 
+}
+
+// wallkicks https://tetris.fandom.com/wiki/SRS
+wallKickx = [8][4]
+
+
+
+{( 0, 0)	(-1, 0)	(-1, 1)	( 0,-2)	(-1,-2)}
+
+
+
+function Block(x, y, type){
+    this.x = x; this.y = y; 
+}
